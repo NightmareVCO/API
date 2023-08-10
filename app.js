d201ccc68601cc94f6207eb889c50794c490efa2
Vladimir <vladimircuriel@outlook.com>
2023-08-10 18:51:55 -0400

    Created API

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express(); // Variable con la capacidad de crear una endpoint
+const port = 3000; //todas las conexiones se pueden hacer por el puerto 3000
+
+app.get('/',(req,res) => {
+   // req es la request, la petición
+   // res es la response, la respuesta
+   res.status(200).send('Hello World!');
+});
+
+// Mientras tengamos esto en la terminal, el puerto 3000 esta escuchando, sino se debe de correr con node.
+app.listen(port,() => {
+   console.log(`Server running at http://localhost:${port}`);
+});
